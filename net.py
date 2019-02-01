88d0f2d10ece10eb3cf0832aebd6b6624a888758
Sergey <xxsmotur@gmail.com>
2019-02-01 21:09:17 +0500

    using reshape method at the loss function

--- net.py
+++ net.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from layer import Layer
+from utils import reshape
 
 class Net:
     def __init__(self, width, height, channels):
@@ -13,3 +14,6 @@ class Net:
 def loss(m_plus, m_minus, length):
     max_len = tf.square(tf.maximum(0., m_plus - length))
     max_r = tf.square(tf.maximum(0., length - m_minus))
+    max_len = reshape(max_len, (cfg.batch_size, -1))
+    max_r = reshape(max_r, (cfg.batch_size, -1))
+    
