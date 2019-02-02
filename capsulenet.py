faa6cf68e11940dbb670901fd47801cd8c503be4
Sergey <xxsmotur@gmail.com>
2019-02-02 23:58:38 +0500

    load model into graph

--- capsulenet.py
+++ capsulenet.py
@@ -1,9 +1,12 @@
 from net import Net
-
+import tensorflow as tf
 
 def main():
     '''entry point of the app
     '''
+    tf.logging.info('starting of the app')
     n = Net()
+    sv = tf.train.Supervisor(graph=n.graph, logdir=cfg.logdir, save_model_secs=0)
+
 if __name__ == "__main__":
     pass
