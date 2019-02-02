9d893e59a5fd52cc30ea6857ee0e8e531883d151
Sergey <xxsmotur@gmail.com>
2019-02-03 00:12:17 +0500

    add save_to method

---
+++ save.py
@@ -0,0 +1,31 @@
+import os
+
+def save_to():
+    if not os.path.exists(cfg.results):
+        os.mkdir(cfg.results)
+    if cfg.is_training:
+        loss = cfg.results + '/loss.csv'
+        train_acc = cfg.results + '/train_acc.csv'
+        val_acc = cfg.results + '/val_acc.csv'
+
+        if os.path.exists(val_acc):
+            os.remove(val_acc)
+        if os.path.exists(loss):
+            os.remove(loss)
+        if os.path.exists(train_acc):
+            os.remove(train_acc)
+
+        fd_train_acc = open(train_acc, 'w')
+        fd_train_acc.write('step,train_acc\n')
+        fd_loss = open(loss, 'w')
+        fd_loss.write('step,loss\n')
+        fd_val_acc = open(val_acc, 'w')
+        fd_val_acc.write('step,val_acc\n')
+        return(fd_train_acc, fd_loss, fd_val_acc)
+    else:
+        test_acc = cfg.results + '/test_acc.csv'
+        if os.path.exists(test_acc):
+            os.remove(test_acc)
+        fd_test_acc = open(test_acc, 'w')
+        fd_test_acc.write('test_acc\n')
+        return(fd_test_acc)
