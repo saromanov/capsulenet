4050c127014efd925e855fd05b1249fc67ab99ee
Sergey <xxsmotur@gmail.com>
2019-02-03 00:07:56 +0500

    implementation of train method

--- capsulenet.py
+++ capsulenet.py
@@ -1,6 +1,14 @@
 from net import Net
 import tensorflow as tf
 
+
+def train(model, supervisor, num_label, epoches=100):
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = supervisor.managed_session(config=config)
+    for epoch in range(epochs):
+        if supervisor.should_stop():
+            break
 def main():
     '''entry point of the app
     '''
